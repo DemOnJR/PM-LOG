a6f9be378dc09a6abea0795d43d1580d4a4e5e79
DemOnJR <bogdanpipiasi@yahoo.com>
2021-06-22 16:16:44 +0200

    up

--- game/src/log.h
+++ game/src/log.h
@@ -1,3 +1,7 @@
+// Find
+		void		InvalidServerLog(enum eLocalization eLocaleType, const char* pcszIP, const char* pszRevision);
+
+// Add
 #ifdef ENABLE_LOG_PM
-	void		WhisperLog(const char * from, const char * to, const char * message);
+		void		WhisperLog(const char * from, const char * to, const char * message);
 #endif
